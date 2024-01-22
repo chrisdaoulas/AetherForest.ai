561304b08fccfaf3591b0456f846422bae7868d9
chrisdaoulas <fe13cd@mail.wbs.ac.uk>
2024-01-22 09:39:11 +0100

    fixed bricking and timestamp finally

--- app/dashboard/blockchain/page.tsx
+++ app/dashboard/blockchain/page.tsx
@@ -145,7 +145,7 @@ const handleKeyPress = () => {
                   <tr>
                     <th scope="col" className="px-4 py-5 font-medium sm:pl-6"> BlockNumber</th>
                     <th scope="col" className="px-3 py-5 font-medium">Event Name</th>
-                    <th scope="col" className="px-3 py-5 font-medium">Unix Timestamp</th>
+                    <th scope="col" className="px-3 py-5 font-medium">Timestamp</th>
                     <th scope="col" className="px-3 py-5 font-medium">From</th>
                     <th scope="col" className="px-3 py-5 font-medium">To</th>
                     <th scope="col" className="px-3 py-5 font-medium">Credits/ Cid/ Allowance</th>
@@ -156,40 +156,46 @@ const handleKeyPress = () => {
                 </thead>
                 <tbody className="bg-white">
                 {filteredEvents.map((event, index) => (
+
                   /* LogIndex, transactionIndex, transactionHash, blockHash, blockNumber, 
                  address, data, topics, returnValues, event, signature, raw
                     returnValues keys: 0 1 2 _ _length_ _from to value */
+
                   <tr key={index} className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+
                     <td className="whitespace-nowrap py-3 pl-6 pr-3">{event['data']['blockNumber'].toString()}</td>            
+
                     <td className="whitespace-nowrap px-3 py-3"><strong>{event['name']}</strong></td>
+
                     <td className="whitespace-nowrap px-3 py-3">
-                      {event['name']=='CarbonOffsetsClaimed' && event['data']['returnValues']['timestamp'].toString()}
-                      {event['name']=='CID' && event['data']['returnValues']['timestamp'].toString()}
-                      {event['name']=='Approval' && event['data']['returnValues']['timestamp'].toString()}
-                      {event['name']=='Transfer' && events[index - 1]['data']['returnValues']['timestamp'].toString()}
-                      
-                      
-                      </td>
-                  <td className="whitespace-nowrap px-3 py-3">
+                      {event['name']=='CarbonOffsetsClaimed' && format(fromUnixTime(Number(event['data']['returnValues']['timestamp'].toString())),"dd-MM-yyyy HH:mm:ss xxx")}
+                      {event['name']=='CID' && format(fromUnixTime(Number(event['data']['returnValues']['timestamp'].toString())),"dd-MM-yyyy HH:mm:ss xxx")}
+                      {event['name']=='Approval' && format(fromUnixTime(Number(event['data']['returnValues']['timestamp'].toString())),"dd-MM-yyyy HH:mm:ss xxx")}
+                      {event['name']=='Transfer' && format(fromUnixTime(Number(events[index - 1]['data']['returnValues']['timestamp'].toString())),"dd-MM-yyyy HH:mm:ss xxx")}                                          
+                    </td>
+
+                    <td className="whitespace-nowrap px-3 py-3">
                       {event['data']['returnValues']['from']}
                       {event['name']=='CarbonOffsetsClaimed' && event['data']['returnValues'][0].toString()}
                       {event['name']=='Approval' && event['data']['returnValues'][0].toString()}
-                      {event['name']=='CID' && prevEvents[1]['data']['returnValues'][0].toString()}
-
-
-                      </td>
-                    <td className="whitespace-nowrap px-3 py-3"> 
-                    <strong>{event['data']['returnValues'][1]}</strong>
-                    <strong>{event['name']=='CID' && event['data']['returnValues'][1]}</strong>
-                    <strong>{event['name']=='CID' && events[index+1]['data']['returnValues'][1].toString()}</strong>
+                      {event['name']=='CID' && events[index+1]['data']['returnValues'][0].toString()}
                     </td>
+
+                    <td className="whitespace-nowrap px-3 py-3"> 
+                      <strong>{event['data']['returnValues'][1]}</strong>
+                      <strong>{event['name']=='CID' && event['data']['returnValues'][1]}</strong>
+                      <strong>{event['name']=='CID' && events[index+1]['data']['returnValues'][1].toString()}</strong>
+                    </td>
+                    
                     <td className="whitespace-nowrap px-3 py-3">
                       {event['name']=='CarbonOffsetsClaimed' && event['data']['returnValues'][1].toString().concat(" AMZ")}
                       {event['name']=='Approval' && event['data']['returnValues'][2].toString().concat(" AMZ")}
                       {event['name']=='CID' && event['data']['returnValues']['cid']}
                       {event['name']=='Transfer' && event['data']['returnValues'][2].toString().concat(" AMZ")}
                     </td>
+
                     <td className="whitespace-nowrap px-3 py-3"><strong>{event['data']['blockHash']}</strong></td>
+
                     <td className="whitespace-nowrap px-3 py-3">{event['data']['transactionHash']}</td>
 
                     </tr>
