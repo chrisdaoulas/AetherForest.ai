7489d23bad3087ee5a16a7ff48138b87d8a82a8e
chrisdaoulas <fe13cd@mail.wbs.ac.uk>
2024-02-09 20:23:57 +0100

    fixed user roles

--- auth.ts
+++ auth.ts
@@ -9,46 +9,73 @@ import { useUser } from './app/login/user';
 
 
 
+
 async function getUser(email: string): Promise<User | undefined> {
-    try {
+  try {
 
-      const { dispatch } = useUser();
+    const { dispatch } = useUser();
 
-      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      const updatedUser = user.rows[1] === 'public' ? { ...user, role: 'beneficiary' } : user;
+    //const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    const userQueryResult = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+
+    // Ensure that the queryResult is not empty
+    if (userQueryResult.rows && userQueryResult.rows.length > 0) {
+      const user = userQueryResult.rows[0];
+
+      const updatedUser = {
+        ...user,
+        role: determineUserRole(user.name.toString()),
+      };
+    
+
       dispatch({ type: 'SET_USER', payload: updatedUser });
-      return user.rows[1];
-      
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-    }
+
+    //return user.rows[0];
+    return userQueryResult.rows[0];
+  }}     
+
+   catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+
+function determineUserRole(name: string): 'public' | 'consortium' | 'beneficiary' {
+  if (name === 'Public') {
+    return 'public';
+  } else if (name === 'Consortium') {
+    return 'consortium';
+  } else if (name === 'Beneficiaries') {
+    return 'beneficiary';
   }
 
+  return 'public';
+
+}
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [Credentials({
     async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string() })
-          .safeParse(credentials);
+      const parsedCredentials = z
+        .object({ email: z.string().email(), password: z.string() })
+        .safeParse(credentials);
 
-          if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
+      if (parsedCredentials.success) {
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        if (!user) return null;
 
-            //const passwordsMatch = password===user.password;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
+        //const passwordsMatch = password===user.password;
+        const passwordsMatch = await bcrypt.compare(password, user.password);
 
-            if (passwordsMatch) return user;
-            console.log(user.toString())
+        if (passwordsMatch) return user;
+        console.log(user.toString())
 
-          }
-          console.log('Invalid credentials');
-          return null;
-        },
-    }),
+      }
+      console.log('Invalid credentials');
+      return null;
+    },
+  }),
   ],
 })
